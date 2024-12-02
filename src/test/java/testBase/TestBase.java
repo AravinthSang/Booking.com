d0695fdec45c64ce01c97b0f33700c94579638e9
AravinthSang <aravinthproj@gmail.com>
2024-12-02 21:43:03 +0530

    added selenium grid setup

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -4,17 +4,18 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -26,18 +27,13 @@ public class TestBase {
 	@Parameters("browser")
 	public void setup(String browser) throws IOException {
 
-		switch (browser.toLowerCase()) {
-		case "edge":
-			driver = new EdgeDriver();
-			break;
-		case "chrome":
-			driver = new ChromeDriver();
-			break;
-		case "firefox":
-			driver = new FirefoxDriver();
-			break;
-
-		}
+		DesiredCapabilities dc=new DesiredCapabilities();
+		dc.setBrowserName(browser);
+		dc.setPlatform(Platform.WIN10);
+		
+		URL hub=new URL("http://localhost:4444");
+		driver=new RemoteWebDriver(hub,dc);
+		
 		FileReader file=new FileReader("./src/test/resources/config.properties");
 		properties=new Properties();
 		properties.load(file);
